190f814ed36e7b9baddd8db459542184ab351c44
RAJ SINHA <33878640+RajSinha77@users.noreply.github.com>
2021-05-25 14:45:21 +0530

    Create Constants.java

---
+++ app/src/main/java/com/noobsever/codingcontests/Utils/Constants.java
@@ -0,0 +1,25 @@
+package com.noobsever.codingcontests.Utils;
+
+public class Constants {
+
+    public static final String TAB_ITEMS_PREFERENCES_KEY = "TAB_ITEMS_PREFERENCES_KEY";
+    public static final String TAB_ITEMS_ARRAYLIST_KEY = "TAB_ITEMS_ARRAYLIST_KEY";
+    public static final String CODEFORCES = "Codeforces";
+    public static final String CODECHEF = "CodeChef";
+    public static final String HACKERRANK = "HackerRank";
+    public static final String HACKEREARTH = "HackerEarth";
+    public static final String SPOJ = "SPOJ";
+    public static final String ATCODER = "AtCoder";
+    public static final String LEETCODE = "LeetCode";
+    public static final String GOOGLE = "Google Coding Competitions";
+    public static final String LAYOUT_SWITCH_KEY = "LAYOUT_SWITCH_KEY";
+    public static final String CURRENT_ACTIVITY = "CURRENT_ACTIVITY";
+    public static final String SWITCH_TWELVE = "SWITCH_TWELVE";
+    public static final String SWITCH_NOTIFICATION = "SWITCH_NOTIFICATION";
+    public static final String CONTEST_OBJECT_TABLE_NAME = "CONTEST_OBJECT_TABLE";
+    public static final String ROOM_DB_NAME = "CONTEST_DB";
+    //need to be changed API_URL
+    public static final String API_URL = "https://hnccbits.com/";
+    public static final String WEBSITE = "website";
+    public static final String ISINTERNET = "isInterNet";
+}
