993cd64394582eedbb2275226c546af210d97455
Nipun <nipunvirat0@gmail.com>
2021-07-08 21:56:07 +0530

    set Faq Fragment

--- app/src/main/java/com/noobsever/codingcontests/ui/slideshow/FAQsFragment.java
+++ app/src/main/java/com/noobsever/codingcontests/ui/slideshow/FAQsFragment.java
@@ -46,12 +46,12 @@ public class FAQsFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         helpObjectArrayList = new ArrayList<>();    //This ArrayList contains question,answers and an ArrowId which determines the View Visibility
-        helpObjectArrayList.add(new HelpObject("Why this app?","To remind coders about different coding contests ",0));
+        helpObjectArrayList.add(new HelpObject("Why this app?","To remind coders about different coding contests "));
         //helpObjectArrayList.add(new HelpObject("What is the tech stack?","Android, Java, Retrofit, Firebase",0));
-        helpObjectArrayList.add(new HelpObject("Is it open source project?","Yes, you can visit HnCC website for more details",0));
-        helpObjectArrayList.add(new HelpObject("Is the app free to use?","Yes it's completely free",0));
-        helpObjectArrayList.add(new HelpObject("How many coding platforms does the app support","The app supports 8 coding platforms",0));
-        helpObjectArrayList.add(new HelpObject("Can I make any changes to the app","yes send a request in the github",0));
+        helpObjectArrayList.add(new HelpObject("Is it open source project?","Yes, you can visit HnCC website for more details"));
+        helpObjectArrayList.add(new HelpObject("Is the app free to use?","Yes it's completely free"));
+        helpObjectArrayList.add(new HelpObject("How many coding platforms does the app support","The app supports 8 coding platforms"));
+        helpObjectArrayList.add(new HelpObject("Can I make any changes to the app","yes send a request in the github"));
 
         helpAdapter = new HelpAdapter(context,helpObjectArrayList);
         binding.helpRecyclerView.setLayoutManager(new LinearLayoutManager(context));
